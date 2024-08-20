a70e01b5bd6284c1f6194c209f026a4bda4a4f51
abreuamalan <abreuaalan@gmail.com>
2024-08-19 19:53:27 -0500

    Graphics

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import plotly.express as px
 data = pd.read_csv('vehicles_us.csv')
 print(data.head(5))
 
+st.title('Project 6: Web Aplication Creation (Data Scientist)')
+
 odo_button = st.button('Odometer Histogram')
-p_o_button = st.button('Price per  # Cylinders')
+p_o_button = st.button('Price per Odometer')
 car_type_button = st.button('Car Type Histogram')
 
 if odo_button:
@@ -15,15 +17,13 @@ if odo_button:
     fig = px.histogram(data, x='odometer', title='Odometer Histogram')
     st.plotly_chart(fig)
 
+
 if p_o_button:
-    st.write('Price per #Cylinders')
-    fig_2 = px.scatter(data, x="cylinders", y="price", title='Price per #Cylinders') 
+    st.write('Price per Odometer')
+    fig_2 = px.scatter(data, x="odometer", y="price", title='Price per Odometer') 
     st.plotly_chart(fig_2)
 
 if car_type_button:
     st.write('Car Type Histogram')
     fig_3 = px.histogram(data, x='type', title='SUV, Sedan and Truck are the most car types of this analysis' )
     st.plotly_chart(fig_3)
-
-
-
